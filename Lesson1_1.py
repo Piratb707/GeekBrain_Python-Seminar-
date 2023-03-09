c4013be55388fe8d886a281f082cf33a1fa7d6a2
Your Name <beloshapkobm@yandex.ru>
2023-03-09 20:38:51 -0300

    "First task update"

--- Lesson1_1.py
+++ Lesson1_1.py
@@ -8,6 +8,27 @@
 # 13579 >>> Сумма чисел числа 13579 равна 25(1 + 3 + 5 + 7 + 9)
 
 # num = 767
-num = input("Введите число :>> ")
-sum_of_digits = int(num[0]) + int(num[1]) + int(num[2])
-print(f"Сумма чисел {num} равна: {sum_of_digits}")
+# num = input("Введите число :>> ")
+# sum_of_digits = int(num[0]) + int(num[1]) + int(num[2])
+# print(f"Сумма цифр числа {num} равна: {sum_of_digits}")
+
+# Усложнение 1 Решите для числа произвольной разрядности: произвольное количество цифр в числе.
+
+# num = input("Введите число:>> ")
+# digit_sum = 0
+# for digit in num:
+#     digit_sum += int(digit)
+# print(f"Сумма цифр числа {num} равна: {digit_sum}")
+
+
+# Усложнение 2 Для числа произвольной разрядности добавить в вывод строку с числами.
+
+# num = input("Введите число:>> ")
+# digit_sum = 0
+# digits = []
+# for digit in num:
+#     digit_sum += int(digit)
+#     digits.append(digit)
+# digit_str = " + ".join(digits)
+# print(f"Сумма цифр числа {num} равна: {digit_sum} ({digit_str})")
+
